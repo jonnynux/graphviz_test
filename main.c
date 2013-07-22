674ce028c3edfa3239f214e15bc130d62e059bb1
jonnynux <jonny84.gdp@gmail.com>
2013-07-22 10:09:49 +0200

    created netbeans project

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jonny
+ *
+ * Created on July 22, 2013, 10:01 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
